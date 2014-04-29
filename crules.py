493a544509d0dff70ce2f29a12362edbd6fe79df
slavik <slavik81@gmail.com>
2014-04-29 02:53:44 -0600

    Add docstrings to crule.py

--- crules.py
+++ crules.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 
 def guardSymbol(fileName):
+	'''The expected include guard symbol for a given file.
+
+	Returns an include guard symbol as a string.
+	fileName is the name of the file being guarded,
+	and does not include any directory names.
+	'''
 	return fileName.upper().replace('.', '_')
 
 def isHeaderFile(fileName):
+	'''Returns true if the given file is identified as a C/C++ header file.'''
 	return fileName.endswith('.h') or fileName.endswith('.hpp')
