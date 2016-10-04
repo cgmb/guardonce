cgmb/guardonce

commit 8fa9eca525fa01a532d598cd6de5fda32e29667b

More minor cleanup of arguments

guardonce/checkguard.py
@@ -161,7 +161,8 @@ def main():
             help='recursively search directories for headers')
     parser.add_argument('-p','--pattern',
             default=None,
-            help='check that include guards must match the specified pattern')
+            metavar='pattern',
+            help='check that include guards match the specified pattern')
     parser.add_argument('-e','--exclude',
             action='append',
             dest='exclusions',
@@ -169,10 +170,11 @@ def main():
             default=[],
             help='exclude files that match the given pattern')
     parser.add_argument('-o','--only',
-            dest='types',
-            metavar='types',
+            dest='type',
+            metavar='type',
             default='any',
-            help='only accept specified types of include proction')
+            choices=['guard','once','g','o'],
+            help='only accept the specified type of include protection')
     args = parser.parse_args()
 
     class Options:

commit eaca635a52d15a2e0c5a16b095ad45f51afa9b13

Fix arguments to --only
Whoops. This was broken in 8fa9eca525fa01a532d598cd6de5fda32e29667b

guardonce/checkguard.py
@@ -180,8 +180,8 @@ def main():
     class Options:
         pass
     options = Options()
-    options.guardOk = args.types in ['g', 'guard', 'any']
-    options.onceOk = args.types in ['o', 'once', 'any']
+    options.guardOk = args.type in ['g', 'guard', 'any']
+    options.onceOk = args.type in ['o', 'once', 'any']
     options.createGuard = processGuardPattern(args.pattern)
 
     for f in args.files: