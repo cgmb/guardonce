46b417d3169c4be55e42c290cdb6344214944378
Cory Bloor <cgbloor@ucalgary.ca>
2017-10-10 20:21:21 -0600

    Fix integration tests on Windows

--- tests/integration/test_checkguard.py
+++ tests/integration/test_checkguard.py
@@ -5,6 +5,7 @@
 from nose.tools import *
 from subprocess import Popen, PIPE
 import collections
+import os
 import shlex
 import sys
 
@@ -23,6 +24,14 @@ def ds(s):
         return s
     return s.decode()
 
+def w(s):
+    """
+    Wrap a given string to be expected output
+    """
+    if s:
+        return s + os.linesep
+    return s
+
 def quickcall(*args):
     cmd = [sys.executable, '-m']
     for arg in args:
@@ -40,14 +49,14 @@ def quickcall(*args):
 def test_utf8_with_bom_guard():
     path = 'tests/data/utf8-bom-guard.h'
     stdout, stderr, exitcode = quickcall(checkguard, '-o guard', path)
-    assert_equal(stdout, '')
+    assert_equal(stdout, w(''))
 
 def test_utf8_with_bom_once():
     path = 'tests/data/utf8-bom-once.h'
     stdout, stderr, exitcode = quickcall(checkguard, '-o once', path)
-    assert_equal(stdout, '')
+    assert_equal(stdout, w(''))
 
 def test_utf8_with_bom_unprotected():
     path = 'tests/data/utf8-bom-unprotected.h'
     stdout, stderr, exitcode = quickcall(checkguard, path)
-    assert_equal(stdout, path + '\n')
+    assert_equal(stdout, w(path))
