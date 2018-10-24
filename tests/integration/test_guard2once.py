a0b4bc70cd9b2bef28faa83b4c59e519d129ead8
Cory Bloor <cgbloor@ucalgary.ca>
2018-10-24 03:02:16 -0600

    Skip integration tests on Windows for now

--- tests/integration/test_guard2once.py
+++ tests/integration/test_guard2once.py
@@ -2,25 +2,31 @@
 # Copyright (C) 2017-2018 Cordell Bloor
 # Published under the MIT License
 
+import os
 from nose.tools import *
+from unittest import skipIf
 from .util import (quickcall, w, with_sandbox, contents_of)
 
 guard2once = 'guardonce.guard2once'
+is_windows = os.name == 'nt'
 
 def test_help():
     stdout, stderr, exitcode = quickcall(guard2once, '--help')
     assert_equal(exitcode, 0)
 
+@skipIf(is_windows, 'work in progress')
 @with_sandbox('tests/data/utf8-bom-guard.h')
 def test_utf8_with_bom_guard_stdout(path, **kwargs):
     stdout, stderr, exitcode = quickcall(guard2once, '-s --stdout', path)
     assert_equal(stdout, contents_of('tests/data/utf8-bom-once.h'))
 
+@skipIf(is_windows, 'work in progress')
 @with_sandbox('tests/data/utf8-bom-once.h')
 def test_utf8_with_bom_once(path, **kwargs):
     stdout, stderr, exitcode = quickcall(guard2once, '-s --stdout', path)
     assert_equal(stdout, contents_of('tests/data/utf8-bom-once.h'))
 
+@skipIf(is_windows, 'work in progress')
 @with_sandbox('tests/data/utf8-bom-unprotected.h')
 def test_utf8_with_bom_unprotected(path, **kwargs):
     stdout, stderr, exitcode = quickcall(guard2once, '-s --stdout', path)
